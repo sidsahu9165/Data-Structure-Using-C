26375555a0c82c8377c7d3f2bfea2c143607deec
sidsahu9165 <109733158+sidsahu9165@users.noreply.github.com>
2022-08-04 04:07:56 -0700

    04/08/2022

---
+++ strlen_function_code.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char s[40];
+int l;
+printf("Enter any string:");
+scanf("%s",&s);
+l = strlen(s);
+printf("The Length of string is %d",l);
+}
